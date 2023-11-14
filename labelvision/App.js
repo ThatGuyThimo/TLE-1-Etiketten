93ee7f497b637511402c1db7516cae046080b068
Thimo <thimodehaan@gmail.com>
2023-11-14 12:55:32 +0100

    Created component navbar WIP

--- labelvision/App.js
+++ labelvision/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from "./js/components/navbar"
 
 export default function App() {
   return (
+    <View>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Ik ga even een lekker bakkie koffie zetten!</Text>
+      <Text>Ik ga even een lekker bakkie koffie zettendd!</Text>
       <StatusBar style="auto" />
+    </View>
+      <Navbar/>
     </View>
   );
 }
