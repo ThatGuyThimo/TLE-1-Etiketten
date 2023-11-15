80df67ec024a38bf9767ef6cdd8e5273b8cd4efc
Thimo <thimodehaan@gmail.com>
2023-11-15 21:13:34 +0100

    WIP Working navbar with page changing and icon's. Created better structure.

---
+++ labelvision/pages/Camera.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Camera() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Camera!</Text>
+      </View>
+    );
+  }
+
+  export {Camera}
