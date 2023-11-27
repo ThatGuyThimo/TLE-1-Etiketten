ac8572aad892d31b9cdaebbc2bc9c3937df0bf33
Thimo <thimodehaan@gmail.com>
2023-11-27 23:52:51 +0100

    working switch button

--- labelvision/pages/Filters.js
+++ labelvision/pages/Filters.js
@@ -1,9 +1,30 @@
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, Switch } from 'react-native';
 
 function Filters() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState)
+  
+  };
+  let text = "off"
+  function switchEnabled() {
+    if (isEnabled) {
+      text =  'on!'
+    }
+  }
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Filters!</Text>
+        <Text>{text}</Text>
+        <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
       </View>
     );
   }
