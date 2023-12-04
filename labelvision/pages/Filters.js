6c085ab1b196f9c4f13e0192aab54693a3306325
Atal09 <113996359+Atal09@users.noreply.github.com>
2023-12-04 21:56:47 +0100

    searchbar, switch

--- labelvision/pages/Filters.js
+++ labelvision/pages/Filters.js
@@ -1,32 +1,63 @@
-import React, {useState} from 'react';
-import { Text, View, Switch } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Switch, TextInput } from 'react-native';
 
 function Filters() {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => {
-    setIsEnabled(previousState => !previousState)
-  
-  };
-  let text = "off"
-  function switchEnabled() {
-    if (isEnabled) {
-      text =  'on!'
-    }
-  }
+    const [searchText, setSearchText] = useState('');
+    const [HalalEnabled, setHalalEnabled] = useState(false);
+    const [VegetarierEnabled, setVegetarierEnabled] = useState(false);
+    const [NotenAllergieEnabled, setNotenAllergieEnabled] = useState(false);
+    const [LactoseIntolerantieEnabled, setLactoseIntolerantieEnabled] = useState(false);
+
+    const toggleHalalSwitch = () => {
+        setHalalEnabled((previousState) => !previousState);
+    };
+
+    const toggleVegetarierSwitch = () => {
+        setVegetarierEnabled((previousState) => !previousState);
+    };
+
+    const toggleNotenAllergieSwitch = () => {
+        setNotenAllergieEnabled((previousState) => !previousState);
+    };
+
+    const toggleLactoseIntolerantieSwitch = () => {
+        setLactoseIntolerantieEnabled((previousState) => !previousState);
+    };
+
+    const switches = [
+        { label: 'Halal', value: HalalEnabled, toggle: toggleHalalSwitch },
+        { label: 'Vegetariër', value: VegetarierEnabled, toggle: toggleVegetarierSwitch },
+        { label: 'Noten Allergie', value: NotenAllergieEnabled, toggle: toggleNotenAllergieSwitch },
+        { label: 'Lactose Intolerantie', value: LactoseIntolerantieEnabled, toggle: toggleLactoseIntolerantieSwitch },
+    ];
+
+    const filteredSwitches = switches.filter((switchItem) =>
+        switchItem.label.toLowerCase().includes(searchText.toLowerCase())
+    );
 
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Filters!</Text>
-        <Text>{text}</Text>
-        <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-      </View>
-    );
-  }
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 20, padding: 10 }}
+                onChangeText={(text) => setSearchText(text)}
+                value={searchText}
+                placeholder="Search..."
+            />
+            {filteredSwitches.map((switchItem) => (
+                <View key={switchItem.label}>
+                    <Text>{switchItem.label}</Text>
+                    <Switch
+                        trackColor={{ false: '#767577', true: '#81b0ff' }}
+                        thumbColor={switchItem.value ? '#f5dd4b' : '#f4f3f4'}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={switchItem.toggle}
+                        value={switchItem.value}
 
-  export {Filters}
+                    />
+                </View>
+            ))}
+        </View>
+    );
+}
+
+export { Filters };
