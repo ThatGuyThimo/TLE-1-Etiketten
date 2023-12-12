cbd27f6e9f949e6ced7f0c7fbaac2e4973c3784d
fredsjr <Oli4schell@gmail.com>
2023-12-12 13:20:19 +0100

    added style to filters, a couple bugs need to be fixed like the switch buttons.

--- labelvision/pages/Filters.js
+++ labelvision/pages/Filters.js
@@ -1,32 +1,91 @@
-import React, {useState} from 'react';
-import { Text, View, Switch } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Switch, Image } from 'react-native';
+import styleSheet from 'react-native-web/src/exports/StyleSheet';
 
 function Filters() {
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => {
-    setIsEnabled(previousState => !previousState)
-  
+    setIsEnabled((previousState) => !previousState);
   };
-  let text = "off"
-  function switchEnabled() {
-    if (isEnabled) {
-      text =  'on!'
-    }
-  }
 
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Filters!</Text>
-        <Text>{text}</Text>
-        <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+  return (
+      <View style={styles.container}>
+        <View style={styles.switchContainer}>
+          <Image
+              source={require('../assets/lactose.png')}
+              style={styles.imageStyle}
+          />
+          <Text style={styles.filterText}>Lactose!</Text>
+          <View style={styles.switchButtonContainer}>
+          <Switch
+              trackColor={{ false: '#E1E1E1', true: '#F06B05' }}
+              thumbColor={isEnabled ? '#0A3D4C' : '#0A3D4C'}
+              ios_backgroundColor="#E1E1E1"
+              style={{ transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }] }}
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+          />
+        </View>
+        </View>
+
+        <View style={styles.switchContainer}>
+          <Image
+              source={require('../assets/lactose.png')}
+              style={styles.imageStyle}
+          />
+          <Text style={styles.filterText}>Gluten!</Text>
+          <View style={styles.switchButtonContainer}>
+          <Switch
+              trackColor={{ false: '#E1E1E1', true: '#F06B05' }}
+              thumbColor={isEnabled ? '#0A3D4C' : '#0A3D4C'}
+              ios_backgroundColor="#E1E1E1"
+              style={{ transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }] }}
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+          />
+        </View>
       </View>
-    );
-  }
+      </View>
+  );
+}
 
-  export {Filters}
+const styles = styleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '80%',
+    height: 100,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 30,
+    padding: 30,
+    marginTop: 10,
+
+  },
+
+  switchButtonContainer: {
+    flex: 1,
+    alignItems: 'flex-end', // Align the switch to the right
+  },
+
+  imageStyle: {
+    width: 60,
+    height: 60,
+    marginRight: 10,
+  },
+
+  filterText: {
+    color: '#F06B05',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
+
+export { Filters };
